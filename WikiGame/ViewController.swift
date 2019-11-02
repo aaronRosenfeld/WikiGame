27e9900d07025462939118635b67f9404fcc4f41
aaronRosenfeld <aaron.rosenfeld.1@gmail.com>
2019-11-01 23:14:20 -0400

    got async functions working and directing webpage to article1

--- WikiGame/ViewController.swift
+++ WikiGame/ViewController.swift
@@ -24,6 +24,10 @@ struct Random: Decodable{
     let title: String
 }
 
+var randomArticle1: String = ""
+var randomArticle2: String = ""
+
+
 class ViewController: UIViewController {
    
     @IBOutlet weak var timeLabel: UILabel!
@@ -34,34 +38,44 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var webView: WKWebView!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        var randomArticle1: String = ""
-        var randomArticle2: String = ""
+
+        getRandomArticles { resp, err in
+            DispatchQueue.main.async {
+                if let resp = resp {
+                    randomArticle1 = resp.query.random[0].title
+                    randomArticle2 = resp.query.random[1].title
+                }
+                
+                self.articleOneLabel.text = randomArticle1
+                self.articleTwoLabel.text = randomArticle2
+                
+                let viewURL = URL(string: "https://en.wikipedia.org/wiki/\(randomArticle1.replacingOccurrences(of: " ", with: "_"))")!
+                //print(viewURL)
+                let viewRequest = URLRequest(url: viewURL)
+                self.webView.load(viewRequest)
+                
+            }
+        }
+    }
+    
+    func getRandomArticles(randomCompletionHandler: @escaping (RandomResponse?, Error?) -> Void){
         guard let randomURL = URL(string: "http://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=2") else { return }
         URLSession.shared.dataTask(with: randomURL) { (data, response, error) in
             guard let data = data else { return }
             do{
-                let resp = try JSONDecoder().decode(RandomResponse.self, from: data)
-                //print(resp.query.random[0].title)
-                randomArticle1 = resp.query.random[0].title
-                print(randomArticle1)
-                randomArticle2 = resp.query.random[1].title
-                print(randomArticle2)
-                //articleOneLabel.text = randomArticle1
+                if let resp = try JSONDecoder().decode(RandomResponse?.self, from: data){
+                    randomCompletionHandler(resp, nil)
+                }
+//                randomCompletionHandler(resp, nil)
+//                randomArticle1 = resp.query.random[0].title
+//                randomArticle2 = resp.query.random[1].title
             } catch let jsonErr{
                 print("Error:", jsonErr)
+                randomCompletionHandler(nil, jsonErr)
             }
         }.resume()
-        
-        let viewURL = URL(string: "https://www.google.com")!
-        let viewRequest = URLRequest(url: viewURL)
-        webView.load(viewRequest)
-        
     }
 
 
